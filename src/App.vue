bf8b83fc3d78c1a63e22347df07d7811ff120fbe
Noel Marshall <6090726+Polychlorobenzene@users.noreply.github.com>
2020-05-30 09:56:25 -0700

    Initial add grid components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloGrid />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloGrid from './components/HelloGrid.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    HelloGrid,
   },
 })
 export default class App extends Vue {}
---
+++ src/models/IGridPage.ts
@@ -0,0 +1,4 @@
+export default interface IGridPage{
+    page: number
+    isCurrentPage: boolean
+}
